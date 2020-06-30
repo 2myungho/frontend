ccabace004f978b1b0757bfe55c2e7aa6c51c67c
lee myung ho <dl_audgh@naver.com>
2020-06-30 17:15:17 +0900

    로그인 상태 유지하기

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styled,{css} from 'styled-components';
-import palette from '../../lib/styles/palette'
+import {Link} from 'react-router-dom';
+import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
-    border: none;
+const buttonStyle = css`
+    border:none;
     border-radius:4px;
     font-size:1rem;
     font-weight:bold;
@@ -11,11 +12,11 @@ const StyledButton = styled.button`
     color:white;
     outline:none;
     cursor:pointer;
-
     background:${palette.gray[8]};
     &:hover{
         background:${palette.gray[6]}
     }
+
     ${props =>
         props.fullWidth &&
         css`
@@ -35,8 +36,19 @@ const StyledButton = styled.button`
     }
 `
 
-const Button = (props) => {
-    return <StyledButton {...props} />
+const StyledButton = styled.button`
+    ${buttonStyle}
+`
+const StyledLink = styled(Link)`
+    ${buttonStyle}
+`
+
+const Button = props => {
+    return props.to?(
+        <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+    ) : (
+        <StyledButton {...props} />
+    )
 };
 
 export default Button;
