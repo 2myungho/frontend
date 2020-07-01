bdc75ac8c6a70bfc7b65e091fbd6bc161e7b6b0f
lee myung ho <dl_audgh@naver.com>
2020-07-02 00:10:40 +0900

    글쓰기 기능

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtons from '../components/write/WriteActionButtons';
 import Responsive from '../components/common/Responsive';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
-            <TagBox />
+            <EditorContainer />
+            <TagBoxContainer />
+            <WriteActionButtonsContainer />
         </Responsive>
     );
 };
