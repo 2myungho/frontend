e4f07bbe31da5e7bb13cb9977fee19f98579fe65
lee myung ho <dl_audgh@naver.com>
2020-07-04 00:48:10 +0900

    교재 최종

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore,applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga'
-import rootReducer,{rootSaga} from './modules';
-import {tempSetUser,check} from './modules/user'
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import { tempSetUser, check } from './modules/user';
+import { HelmetProvider } from 'react-helmet-async';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
 
 function loadUser() {
-  try{
+  try {
     const user = localStorage.getItem('user');
-    if(!user) return; //로그인 상태가 아니라면 아무것도 안 함
+    if (!user) return; // 로그인 상태가 아니라면 아무것도 안함
 
-    store.dispatch(tempSetUser(user))
+    store.dispatch(tempSetUser(user));
     store.dispatch(check());
   } catch (e) {
-    console.log('localStorage is not working') 
+    console.log('localStorage is not working');
   }
 }
 
@@ -32,10 +36,14 @@ loadUser();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
+
+serviceWorker.unregister();
 
 serviceWorker.unregister();
